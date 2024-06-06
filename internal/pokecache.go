88c8e1efb4c98d06ee8c347ee75d6e83e8022199
ASproson <a.sproson@outlook.com>
2024-06-06 19:50:39 +0100

    Adds Cache fetch routine

--- internal/pokecache.go
+++ internal/pokecache.go
@@ -6,11 +6,47 @@ import (
 )
 
 type Cache struct {
-	mu    sync.Mutex
-	cache map[string]cacheEntry
+	mu       sync.Mutex
+	cache    map[string]cacheEntry
+	interval time.Duration
 }
 
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		cache:    make(map[string]cacheEntry),
+		interval: interval,
+	}
+
+	return c
+}
+
+// Adds new entry to the cache
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Create a new cache entry
+	entry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+
+	c.cache[key] = entry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, exists := c.cache[key]
+
+	if exists {
+		return entry.val, true
+	}
+
+	return nil, false
 }
